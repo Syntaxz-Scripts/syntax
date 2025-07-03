a3026631ce9855133a14781e355ffa5ad87accef
Syntaxz-Scripts <syntacxberror@gmail.com>
2025-07-04 00:07:05 +0800

    Update syntaxzscriptsdemo.lua

--- syntaxzscriptsdemo.lua
+++ syntaxzscriptsdemo.lua
@@ -483,7 +483,7 @@ do
 
     -- Jitter settings
     local JITTER_DISTANCE = 6 -- studs
-    local JITTER_SPEED = 1e6 -- higher = faster
+    local JITTER_SPEED = 5e9 -- higher = faster
     local HUMANOID_PART = "HumanoidRootPart"
 
     local lastJitterOffset = 0
