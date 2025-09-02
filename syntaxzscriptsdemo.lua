0221e4d1b47d80bbd464cc932b202cd15f7faa02
Syntaxz-Scripts <syntacxberror@gmail.com>
2025-09-02 21:06:39 +0800

    Update syntaxzscriptsdemo.lua

--- syntaxzscriptsdemo.lua
+++ syntaxzscriptsdemo.lua
@@ -1475,8 +1475,8 @@ do
 
     -- Vibrate/Jitter Character
     local jitterBtn = styledBtn(contentParent, 200, 136, 220, "Vibrate (Jitter) Character: OFF", Color3.fromRGB(130, 120, 220))
-    local JITTER_DISTANCE = 10
-    local JITTER_SPEED = 75
+    local JITTER_DISTANCE = 0.2
+    local JITTER_SPEED = 67e67
     local HUMANOID_PART = "HumanoidRootPart"
     local lastJitterOffset = 0
     local function stopJitter()
